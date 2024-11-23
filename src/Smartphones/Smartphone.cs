f45e03cecc0e9b934b0060846a4f90d597a3ed9a
Березкин Александр <3.commm@gmail.com>
2024-11-23 11:33:17 +0300

    Описали класс Smartphone

--- src/Smartphones/Smartphone.cs
+++ src/Smartphones/Smartphone.cs
@@ -8,6 +8,22 @@ namespace Smartphones
 {
     public class Smartphone
     {
+        /// <summary>
+        /// Назавние модели
+        /// </summary>
+        public string nameModel {  get; set; }
+        /// <summary>
+        /// Цена смартфона
+        /// </summary>
+        public decimal price { get; set; }
+        /// <summary>
+        /// Диагональ экрана
+        /// </summary>
+        public decimal screenDiagonal { get; set; }
 
+        /// <summary>
+        /// Свободный конструктор
+        /// </summary>
+        public Smartphone() { }
     }
 }
